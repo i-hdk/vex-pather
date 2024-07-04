d7c63558471e07b60fbf0e585b1a452cb97ea1ac
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2024-07-04 11:45:50 +0800

    drag works with freeze for overlapping points

--- points/Assets/KickScript.cs
+++ points/Assets/KickScript.cs
@@ -13,18 +13,38 @@ public class KickScript : MonoBehaviour
         defaultCircle = GameObject.Find("Circle");
     }
 
+    //freezes all points besides idx
+    public void FreezeAllExcept(int idx)
+    {
+        for(int i = 0; i < points.Count; i++)
+        {
+            if (i == idx) continue;
+            points[i].GetComponent<Point>().SetFreeze(true);
+        }
+    }
+
+    //unfreezes all points
+    public void UnfreezeAll()
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            points[i].GetComponent<Point>().SetFreeze(false);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             points.Add(Instantiate(defaultCircle));
             points[points.Count - 1].AddComponent<Point>();
             var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mouseWorldPos.z = 0f;
-            points[points.Count - 1].GetComponent<Point>().x = mouseWorldPos.x;
-            points[points.Count - 1].GetComponent<Point>().y = mouseWorldPos.y;
-            Debug.Log("pressed");
+            points[points.Count - 1].GetComponent<Point>().SetPosition(mouseWorldPos);
+            points[points.Count - 1].GetComponent<Point>().SetName("point " + (points.Count-1));
+            points[points.Count - 1].GetComponent<Point>().SetIndex(points.Count-1);
+            //Debug.Log("pressed");
         }
     }
 }
--- points/Assets/Point.cs
+++ points/Assets/Point.cs
@@ -1,26 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Point : MonoBehaviour
 {
-    [SerializeField] public float x=0, y=0;
-    public Point(float xpos, float ypos)
-    {
-    }
+    float x=0, y=0;
+    [HideInInspector] string waitSetName = "copy";
+    float radius =  0.5f; //for the default white circle sprite
+    [HideInInspector] bool leftClickPressed = false;
+    int index = -1;
+    bool freeze;
 
     // Start is called before the first frame update
     void Start()
     {
         name = "copy";
+        freeze = false;
+    }
+
+    public void SetName(string newName)
+    {
+        waitSetName = newName; //will change name in Update()
+    }
+
+    public void SetPosition(Vector3 pos)
+    {
+        x = pos.x; 
+        y = pos.y;
+    }
+
+    public void SetIndex(int idx)
+    {
+        index = idx;
+    }
+
+    public void SetFreeze(bool f)
+    {
+        freeze = f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = Color.white;
-        //spriteRenderer.sprite = Resources.LoadAll<Sprite>("circle")[0];
         GetComponent<Transform>().position = new Vector2(x, y);
+        name = waitSetName;
+
+        //look for drag
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        bool mouseOnCircle = Math.Pow((mouseWorldPos.x - x), 2) + Math.Pow((mouseWorldPos.y - y), 2) <= Math.Pow(radius, 2);
+        if (Input.GetMouseButtonDown(0)&&mouseOnCircle&&!freeze)
+        {
+            leftClickPressed = true;
+            Debug.Log("pressed " + name);
+            GameObject.Find("GameObject").GetComponent<KickScript>().FreezeAllExcept(index);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            if (leftClickPressed&&!freeze)
+            {
+                SetPosition(mouseWorldPos);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            leftClickPressed=false;
+            GameObject.Find("GameObject").GetComponent<KickScript>().UnfreezeAll();
+        }
+        else
+        {
+            leftClickPressed= false;
+            GameObject.Find("GameObject").GetComponent<KickScript>().UnfreezeAll();
+        }
     }
 }
